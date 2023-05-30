1b4183912ec7a499a1d20a307b403c18f8d92edd
RamonSantosDev <117384857+RamonSantosDev@users.noreply.github.com>
2023-05-30 08:52:07 -0300

    Add files via upload

---
+++ ManipulandoTextNome.py
@@ -0,0 +1,7 @@
+nome = input('Seu Nome: ')
+div = nome.split()
+print(nome.upper()) #Nome em Maiusculo
+print(nome.lower()) #Nome em Minusculo
+print(len(nome.strip()))  #Quantas letras ao todo(sem considerar espaços)
+print(len(div[0])) #Quantas letras do 1° Nome
+
